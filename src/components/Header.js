5fc21de633b5347294140a5cf3333249fd54d219
FeliOdras <ariane.feldmann@odras.de>
2019-05-22 12:06:20 +0200

    Removed Header content from App.js to Header

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="App-header">
+
+            <p>
+                Edit <code>src/App.js</code> and save to reload.</p>
+            <a
+                className="App-link"
+                href="https://bookmark-api.fbw-11.now.sh/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <button type="button" className="btn btn-primary"><i className="fab fa-chrome"></i> Bookmark API v1.0</button>
+
+            </a>
+        </header>
+    )
+}
+
+export default Header
+
+
+
