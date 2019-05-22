9ab2fb15d5a67d43fc5e29a8d76fa49bb5bfc3b7
AD <atar.dci@gmail.com>
2019-05-22 22:26:27 +0200

    Create dynamic form template for login/sign up according to current state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export default class Form extends Component {
+
     render() {
+        console.log(this.props)
+        const { registered, token } = this.props
         return (
             <div>
-                Login /registration form
-            </div>
+                <h2>{registered && "Login" || "Sign up"}</h2>
+                <form>
+                    <label htmlFor="username">Enter username</label>
+                    <br />
+                    <input id="username" name="username" type="text" />
+                    <br />
+                    <label htmlFor="password">Enter your password</label>
+                    <br />
+                    <input id="password" name="password" type="text" />
+                    <br />
+                    <div>
+                        {registered && <Link to="/"><button type="submit">Log in</button></Link> || <Link to="/"><button type="submit">Sign Up</button></Link>}
+                    </div>
+                </form>
+            </div >
         )
     }
 }
