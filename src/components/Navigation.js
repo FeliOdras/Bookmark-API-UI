6cfa875589aa6789b7ea35e972eded0c274340a7
FeliOdras <ariane.feldmann@odras.de>
2019-05-22 12:20:20 +0200

    Added static links to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <nav>
-
+            <ul className="menu">
+                <li><a href="/">Info</a></li>
+                <li><a href="/">Bookmarks</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
         </nav>
     )
 }
