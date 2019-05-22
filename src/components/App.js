68b31b90f2fcc1244bffac2f44d73c618690df52
AD <atar.dci@gmail.com>
2019-05-22 12:37:22 +0200

    Set app router, redirect user to login page when not already logged in

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import logo from '../styles/logo.svg';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Form from "./Form";
+import Bookmarks from "./Bookmarks";
 import '../styles/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://bookmark-api.fbw-11.now.sh/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button type="button" className="btn btn-primary"><i className="fab fa-chrome"></i> Bookmark API v1.0</button>
+class App extends React.Component {
+  state = {
+    registered: false,
+    loggedIn: false
+  }
 
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Router>
+
+          <Switch>
+            <Route exact path="/" render={(props) => (
+              this.state.loggedIn ? (
+                <Bookmarks {...props} />
+              ) : (
+                  <Redirect to="/login" />
+                )
+            )} />
+            <Route to="/login" render={(props) => <Form {...props} registered={this.state.registered} />} />
+          </Switch>
+
+        </Router>
+      </div>
+    );
+  }
+
 }
 
 export default App;
