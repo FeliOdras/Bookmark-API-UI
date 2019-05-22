50e9de8df2f78d56fd5ab061be0b52a32e9a9ce8
FeliOdras <ariane.feldmann@odras.de>
2019-05-22 12:08:21 +0200

    Edited Header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,8 @@ const Header = () => {
     return (
         <header className="App-header">
 
-            <p>
-                Edit <code>src/App.js</code> and save to reload.</p>
-            <a
-                className="App-link"
-                href="https://bookmark-api.fbw-11.now.sh/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <button type="button" className="btn btn-primary"><i className="fab fa-chrome"></i> Bookmark API v1.0</button>
-
-            </a>
+            <h1 className="h4 p-2">
+                Bookmarks</h1>
         </header>
     )
 }
