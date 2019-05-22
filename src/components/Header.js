9b50da54a3d1b2206e93778160aaccb024a12d82
FeliOdras <ariane.feldmann@odras.de>
2019-05-22 12:13:30 +0200

    Created and integrated navigation container to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navigation from './Navigation';
 
 const Header = () => {
     return (
@@ -6,6 +7,7 @@ const Header = () => {
 
             <h1 className="h4 p-2">
                 Bookmarks</h1>
+            <Navigation />
         </header>
     )
 }
