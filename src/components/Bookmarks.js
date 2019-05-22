da7e5914d3e12bd80d54570e950a140404781b01
AD <atar.dci@gmail.com>
2019-05-22 12:36:51 +0200

    Add placeholder components: Bookmarks and Form

---
+++ src/components/Bookmarks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Bookmarks extends Component {
+    render() {
+        return (
+            <div>
+                Place some bookmarks here
+            </div>
+        )
+    }
+}
