d9449f2446dbf21669177be8e1fdb37036dc090a
FeliOdras <ariane.feldmann@odras.de>
2019-05-22 12:30:17 +0200

    Added flexbox display to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Navigation = () => {
     return (
         <nav>
-            <ul className="menu">
-                <li><a href="/">Info</a></li>
-                <li><a href="/">Bookmarks</a></li>
-                <li><a href="/">Login</a></li>
+            <ul className="menu d-flex justify-content-around p-0 m-0">
+                <li className="p-2"><a href="/">Info</a></li>
+                <li className="p-2"><a href="/">Bookmarks</a></li>
+                <li className="p-2"><a href="/">Login</a></li>
             </ul>
         </nav>
     )
