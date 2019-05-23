5f487ed35dffebb5b995d748ed6386df9a7465bd
FeliOdras <ariane.feldmann@odras.de>
2019-05-23 13:27:57 +0200

    Added Article container for main content

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,9 +4,11 @@ export default function Info() {
     return (
         <div>
             <h2 className="main-heading">Info</h2>
-            <p>This app helps you to organize and manage your internet bookmarks.</p>
-            <p>API: <em>FbW-11 @<a href="https://digitalcareerinstitute.org" target="_blank">DCI</a></em><br />
-                UI: <em>FbW-12 <strong>#hotpink</strong> @<a href="https://digitalcareerinstitute.org" target="_blank">DCI</a></em></p>
+            <article className="bg-light-trans shadow w-auto d-inline-block m-auto p-3">
+                <p>This app helps you to organize and manage your internet bookmarks.</p>
+                <p>API: <em>FbW-11 @<a href="https://digitalcareerinstitute.org" target="_blank">DCI</a></em><br />
+                    UI: <em>FbW-12 <strong>#hotpink</strong> @<a href="https://digitalcareerinstitute.org" target="_blank">DCI</a></em></p>
+            </article>
         </div>
     )
 }
